62e29d01e6342ed79bd388b4f3c97ee437c52388
MattWPhillips <mantis_rulze@hotmail.com>
2013-08-17 12:04:45 +1000

    First upload

---
+++ LinkedList/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinkedList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.add(77);
+            linkedList.add(10);
+            linkedList.add(20);
+            linkedList.add(5);
+            linkedList.add(432);
+            linkedList.add(2);
+            linkedList.add(9);
+
+            linkedList.DisplayAllNodes();
+        }
+    }
+}
---
+++ LinkedList/Node.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Node
+    {
+        public object data;
+        public Node next;
+
+        public Node()
+        {
+            data = null;
+            next = null;
+        }
+
+        public Node(object data)
+        {
+            this.data = data;
+            next = null;
+        }
+
+        public Node(object data, Node next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    class LinkedList
+    {
+        private Node firstNode;
+        private Node lastNode;
+
+        int numOfNodes;
+
+        public void add(object newData)
+        {
+            Node node = new Node(newData);
+
+            if (firstNode == null)
+            {
+                firstNode = node;
+            }
+            if (lastNode != null)
+            {
+                lastNode.next = node;
+            }
+            lastNode = node;
+            numOfNodes++;
+        }
+
+        public void DisplayAllNodes()
+        {
+            Node node = firstNode;
+
+            while(node != null)
+            {
+                Console.WriteLine(node.data);
+                node = node.next;
+            }
+        }
+    
+    }
+}
